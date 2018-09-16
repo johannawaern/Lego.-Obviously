4183c9492778ea08fb099accfe530a443886610f
Johanna Luks Wærn <johanna@misswaern.se>
2018-09-16 13:12:02 +0200

    pic header

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // Our variables
 const key = "1b679073457842c582f0e77ee085d1e1"
 const url = `https://newsapi.org/v2/everything?excludeDomains=mashable.com,gizmodo.com,theverge.com&q=+lego&language=en&sortBy=relevancy&apiKey=${key}`
-const secondUrl = `http://api.open-notify.org/astros.json`
+const secondUrl = `https://newsapi.org/v2/everything?q=legoland&language=en&apiKey=${key}`
 
-/*
+
 const showAlert= () => {
-  alert ("pay first, read later")
-}*/
+  alert ("CONGRATULATIONS!! You are our first visitor! You just won free Lego for life!")
+}
 
 // Our main function
 const recievedNews = (newsdata) => {
@@ -20,7 +20,7 @@ const recievedNews = (newsdata) => {
           `<div class="hero">
           <img src="${article.urlToImage}"/>
           <h2>${article.title}</h2>
-          <a class=button href=${article.url}>read full story </a>
+          <a class=button href=${article.url} onmouseover="showAlert()">read full story </a>
           </div>`
         }
 
@@ -39,21 +39,26 @@ const recievedNews = (newsdata) => {
 }
 
 
-const recievedSpaceman = (spacedata) => {
+const recievedNews1 = (newsdata) => {
 
 
 	// For each article object from the API, we create a new div in HTML.
-    spacedata.articles.forEach((article) => {
+    newsdata.articles.forEach((article) => {
+      
 
 					//Here we create and add html elements to our html file
-					document.querySelector(".allSpace").innerHTML +=
-            `<div class="space">
-            <p>${people.craft}</p>
-            <p>${people.name}</p>
+					document.querySelector(".legoland").innerHTML +=
+            `<div class="news">
+            <img src="${article.urlToImage}"/>
+            <h3>${article.title}</h3>
+            <a class=button href=${article.url}>read full story </a>
             </div>`
 
     })
+
 }
+
+
 
 
 //Fetch is a built in function in Javascript, it gets the data from the API and tranforms it into Javascript objects – JSON data.
@@ -61,6 +66,7 @@ fetch(url)
   .then(response => response.json())
   .then(recievedNews)
 
+
   fetch(secondUrl)
     .then(response => response.json())
-    .then(recievedSpaceman)
+    .then(recievedNews1)
